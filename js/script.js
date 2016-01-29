f4e0f65a79db5adb8ecb313c140d5bc04426a30e
Nayana Davis <nayana487@gmail.com>
2016-01-29 17:09:26 -0500

    added comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $(".readmore").toggle(function(){
-    $(this).text("Readless").siblings(".show-this-on-click").show();    
+    $(this).text("Readless").siblings(".show-this-on-click").show();
 }, function(){
-    $(this).text("readmore").siblings(".show-this-on-click").hide();    
+    $(this).text("readmore").siblings(".show-this-on-click").hide();
 });
+
+// ND: Remember to put your code within the document ready function
+// ND: So we're looking for three functions here that incorporate the slideup, slideDown, show
+// and hide methods. Let me know if you need help figuring out how to do this.
